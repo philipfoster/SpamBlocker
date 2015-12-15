b76e48ef6b721e9df091f41925635892064a570e
Philip <philipfoster462@gmail.com>
2015-12-14 22:46:48 -0600

    Added GPLv3 headers

--- app/src/main/java/net/pfoster/spamblocker/receiver/CallFilterReceiver.java
+++ app/src/main/java/net/pfoster/spamblocker/receiver/CallFilterReceiver.java
@@ -1,12 +1,31 @@
+/*
+ * Copyright (c) 2015-2015 Philip Foster Jr.
+ * Author: Philip Foster <philipfoster462@gmail.com>
+ *
+ * This file is part of Spam Blocker.
+ *
+ *     Spam Blocker is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Spam Blocker is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Spam Blocker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package net.pfoster.spamblocker.receiver;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
 /**
  * This class is notified by the OS when a call is received, as defined in {@code AndroidManifest.xml}
